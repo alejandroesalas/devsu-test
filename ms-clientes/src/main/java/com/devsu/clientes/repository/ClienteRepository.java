fbabbf420cefb0180a38a8d41093c124184cbe57
alopez <ext.alejandro.lopez@softwareone.com>
2026-06-15 22:31:12 -0500

    ms-clientes Add Client Controller and repository

---
+++ ms-clientes/src/main/java/com/devsu/clientes/repository/ClienteRepository.java
@@ -0,0 +1,12 @@
+package com.devsu.clientes.repository;
+
+import com.devsu.clientes.domain.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
+    Optional<Cliente> findByClienteId(String clienteId);
+    boolean existsByClienteId(String clienteId);
+    boolean existsByIdentificacion(String identificacion);
+}
